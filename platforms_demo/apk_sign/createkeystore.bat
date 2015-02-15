f2426d626848907b414284a0bb45b97b38fad38e
lilili87222 <li_li_li87222@163.com>
2015-02-15 18:46:12 +0800

    upgrade baidu ane

--- platforms_demo/apk_sign/createkeystore.bat
+++
@@ -1,6 +0,0 @@
-SET KEYSTORE_KEYPASS=123456
-SET KEYSTORE_STOREPASS=123456
-SET KEYSTORE_PATH=android.keystore
-SET KEYSTORE_ALIAS=android
-
-keytool -genkey -v -keystore %KEYSTORE_PATH% -alias %KEYSTORE_ALIAS% -keyalg RSA -validity 20000 -storepass %KEYSTORE_STOREPASS% -keypass %KEYSTORE_KEYPASS% -dname "CN=cuo,OU=cuo,O=cuo,L=SH,ST=SH,C=CN"
